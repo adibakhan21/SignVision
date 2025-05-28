77c91f483f5e1c6f7b678505e82f0bad1a8a6dc9
Prabhanshuluvsn <prabhanshu421@gmail.com>
2025-05-29 02:22:34 +0530

    Assignment_1_Updated

--- Assignments/Assignment 1/Prabhanshu_240755/.ipynb_checkpoints/A1_Q_2-checkpoint.py
+++
@@ -1,6 +0,0 @@
-sentence = "The book was interesting because the book covered many topics and the topics discussed in the book were engaging"
-#Converting the Sentence to lower case
-sentence = sentence.lower()
-#splitting the sentence into words at white spaces
-words = sentence.split(" ")
-print(words)
